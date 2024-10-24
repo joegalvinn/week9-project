02d85bd40e170a291eb71c0058608120f8c73a4d
Joseph <joseph.galvin@digisolve.co.uk>
2024-10-24 16:52:37 +0100

    test

--- week9-assignment/src/app/sign-in/[[...sign-in]]/page.js
+++ week9-assignment/src/app/sign-in/[[...sign-in]]/page.js
@@ -1,5 +1,5 @@
 //You can use the SignIn component from clerk
-import { SignIn } from "@clerk/clerk-react";
+import { SignIn } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 export default function SignInPage() {
